da040df7293a4379d90f6aa29f28a8b8e9379e9d
bala-amypo <bala@amypo.in>
2025-12-23 08:56:51 +0000

    User commit

--- src/main/java/com/example/demo/service/CredentialHolderProfileService.java
+++ src/main/java/com/example/demo/service/CredentialHolderProfileService.java
@@ -1,16 +1,7 @@
 package com.example.demo.service;
-
 import com.example.demo.entity.CredentialHolderProfile;
-
-import java.util.List;
-
 public interface CredentialHolderProfileService {
-
     CredentialHolderProfile createHolder(CredentialHolderProfile profile);
-
     CredentialHolderProfile getHolderById(Long id);
-
-    List<CredentialHolderProfile> getAllHolders();
-
-    CredentialHolderProfile updateHolderStatus(Long id, boolean active);
+    CredentialHolderProfile updateStatus(Long id, boolean active);
 }
