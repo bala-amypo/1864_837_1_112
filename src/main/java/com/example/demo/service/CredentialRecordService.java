c4d4db585d18f1ba965acf779851048876c9066f
bala-amypo <bala@amypo.in>
2025-12-26 06:22:07 +0000

    User commit

--- src/main/java/com/example/demo/service/CredentialRecordService.java
+++ src/main/java/com/example/demo/service/CredentialRecordService.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
+
 import com.example.demo.entity.CredentialRecord;
 import java.util.List;
+
 public interface CredentialRecordService {
     CredentialRecord createCredential(CredentialRecord record);
     CredentialRecord updateCredential(Long id, CredentialRecord update);
     List<CredentialRecord> getCredentialsByHolder(Long holderId);
     CredentialRecord getCredentialByCode(String code);
-    List<CredentialRecord> getAllCredentials(); // MUST EXIST
+    List<CredentialRecord> getAllCredentials(); // Required for t61/t62
+    CredentialRecord getById(Long id);
 }
