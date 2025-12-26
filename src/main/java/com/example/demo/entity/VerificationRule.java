6cf9068843d8a74cbaa5a54ae2520a4158928cf7
bala-amypo <bala@amypo.in>
2025-12-26 14:20:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationRule.java
+++ src/main/java/com/example/demo/entity/VerificationRule.java
@@ -3,19 +3,17 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
-@ToString
 public class VerificationRule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
-    private String ruleCode; // e.g., R1, R_UNIQUE
+    private String ruleCode;
 
     private Boolean active = true;
 }
